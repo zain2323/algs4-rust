3165a85423349f124533b40f121243c34ab428d0
zain2323 <zainsiddiqui2323@gmail.com>
2022-04-14 13:39:02 +0500

    Added read_all_strings, read_all_ints, read_all_doubles, read_all_longs and read_all methods in the stdin

--- src/io/stdin.rs
+++ src/io/stdin.rs
@@ -1,6 +1,8 @@
 use std::cell::RefCell;
 use std::io;
-use std::io::BufRead;
+use std::io::{BufRead};
+use std::iter::Map;
+use std::str::Split;
 
 thread_local!(static CACHED_BUFFER: RefCell<InputBuffer> = RefCell::new(initialize_buffer()));
 
@@ -24,19 +26,94 @@ pub fn read_all_lines() -> Vec<String> {
     all_lines
 }
 
+pub fn read_all_strings() -> Vec<String> {
+    let lines = read_all_lines();
+    let mut strings: Vec<String> = vec![];
+
+    for line in lines {
+        let split: Vec<&str> = line.split(' ').collect();
+        for string in split {
+            strings.push(string.to_string());
+        }
+    }
+    strings
+}
+
+pub fn read_all() -> String {
+    let lines = read_all_lines();
+    let mut all_strings = String::new();
+
+    for line in lines {
+        all_strings.push_str(&line);
+        all_strings.push_str("\n");
+    }
+    all_strings.remove(all_strings.len()-1);
+    all_strings
+}
+
+pub fn read_all_ints() -> Vec<i32> {
+    let lines = read_all_lines();
+    let mut all_ints: Vec<i32> = vec![];
+    for line in lines {
+        let split: Vec<&str> = line.split(' ').collect();
+        for int in split {
+            if int.trim() != ""
+            {
+                let val = match int.trim().parse() {
+                    Ok(v) => v,
+                    Err(_) => panic!("Unable to parse the input ({}) to the 32bit integer", int.trim())
+                };
+                all_ints.push(val);
+            }
+        }
+    }
+    all_ints
+}
+
+pub fn read_all_longs() -> Vec<i64> {
+    let lines = read_all_lines();
+    let mut all_longs: Vec<i64> = vec![];
+    for line in lines {
+        let split: Vec<&str> = line.split(' ').collect();
+        for long in split {
+            if long.trim() != ""
+            {
+                let val = match long.trim().parse() {
+                    Ok(v) => v,
+                    Err(_) => panic!("Unable to parse the input ({}) to the 64bit integer", long.trim())
+                };
+                all_longs.push(val);
+            }
+        }
+    }
+    all_longs
+}
+
+pub fn read_all_doubles() -> Vec<f64> {
+    let lines = read_all_lines();
+    let mut all_doubles: Vec<f64> = vec![];
+    for line in lines {
+        let split: Vec<&str> = line.split(' ').collect();
+        for double in split {
+            if double.trim() != ""
+            {
+                let val = match double.trim().parse() {
+                    Ok(v) => v,
+                    Err(_) => panic!("Unable to parse the input ({}) to the 64bit floating point value", double.trim())
+                };
+                all_doubles.push(val);
+            }
+        }
+    }
+    all_doubles
+}
+
 pub fn has_next_line() -> bool {
     let stdin = io::stdin();
     let mut lines_iter = stdin.lock().lines().map(|l| l.unwrap());
     match lines_iter.next() {
         None => { false }
         Some(_) => { true }
-    }
-}
-
-fn initialize_buffer() -> InputBuffer {
-    InputBuffer {
-        lines: read_all_lines(),
-        position: 0,
     }
 }
 
@@ -58,6 +135,14 @@ pub fn is_empty() -> bool {
     ans
 }
 
+fn initialize_buffer() -> InputBuffer {
+    InputBuffer {
+        lines: read_all_lines(),
+        position: 0,
+    }
+}
+
+
 struct InputBuffer {
     lines: Vec<String>,
     position: usize,
@@ -68,6 +153,7 @@ mod tests {
     use crate::io::stdin;
 
     #[test]
-    fn is_empty() {
+    fn read_all_strings() {
+        stdin::read_all_lines();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
 /*
 Tests the io parts of the library.
  */
+use std::io::stdin;
+
 pub mod io;
 
 fn main() {
-    is_empty();
-    is_empty();
-    is_empty();
-    is_empty();
-    is_empty();
-
+//    Use of any of the method to test the stdin functionality.
+//     cargo run < {file.txt}
 }
 
 fn read_line() {
@@ -32,4 +30,28 @@ fn is_empty() {
     println!("{}", boolean);
 }
 
+fn read_all_strings() {
+    let all_strings = io::stdin::read_all_strings();
+    println!("{:?}", all_strings);
+}
+
+fn read_all() {
+    let all_strings = io::stdin::read_all();
+    println!("{}", all_strings);
+}
+
+fn read_all_ints() {
+    let all_ints = io::stdin::read_all_ints();
+    println!("{:?}", all_ints);
+}
+
+fn read_all_longs() {
+    let all_longs = io::stdin::read_all_longs();
+    println!("{:?}", all_longs);
+}
+
+fn read_all_doubles() {
+    let all_doubles = io::stdin::read_all_doubles();
+    println!("{:?}", all_doubles);
+}
 
