62f5af78d1683027634c91653af8a2beb33f48d3
zain2323 <zainsiddiqui2323@gmail.com>
2022-04-20 06:18:42 +0500

    Added knuth shuffling algorithm

--- src/context/mod.rs
+++ src/context/mod.rs
@@ -1,2 +1,3 @@
 pub mod convex_hull;
 pub mod karatsuba_multiplication;
+pub mod shuffle;
---
+++ src/context/shuffle.rs
@@ -0,0 +1,40 @@
+// Implements various shuffling algorithms
+
+use rand::{Rng, thread_rng};
+
+trait Shuffler<T>
+where T: Ord
+{
+    fn shuffle(&mut self, slice: &mut[T]);
+}
+
+pub struct KnuthShuffle;
+impl<T> Shuffler<T> for KnuthShuffle
+where T: Ord
+{
+    fn shuffle(&mut self, slice: &mut[T]) {
+        let n = slice.len();
+        for i in 0..n {
+            let random_num: usize = thread_rng().gen_range(0..i+1);
+            slice.swap(i, random_num);
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn it_shuffles() {
+        let mut slice = vec![1,2,3,4,5];
+        KnuthShuffle.shuffle(&mut slice);
+        assert_ne!(slice, &[1,2,3,4,5]);
+    }
+
+    #[test]
+    fn it_shuffles_with_duplicates() {
+        let mut slice = vec![100,21,12,41,54,90,13,12];
+        KnuthShuffle.shuffle(&mut slice);
+        assert_ne!(slice, &[100,21,12,41,54,90,13,12]);
+    }
+}
