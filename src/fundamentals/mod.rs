e36c2c81ea36faa50445a879f8c699d859b42660
zain2323 <zainsiddiqui2323@gmail.com>
2022-04-20 17:31:23 +0000

    Added quick union

--- src/fundamentals/mod.rs
+++ src/fundamentals/mod.rs
@@ -2,4 +2,5 @@ pub mod bag;
 pub mod heap;
 pub mod indexed_pq;
 pub mod queue;
+pub mod quick_find;
 pub mod stack;
---
+++ src/fundamentals/quick_find.rs
@@ -0,0 +1,79 @@
+pub struct QuickFindUF {
+    id: Vec<usize>,
+    count: usize,
+}
+
+impl QuickFindUF {
+    pub fn new(n: usize) -> QuickFindUF {
+        QuickFindUF {
+            id: Self::initialize_vec(n),
+            count: n,
+        }
+    }
+
+    pub fn count(&self) -> usize {
+        self.count
+    }
+
+    pub fn connected(&self, p: usize, q: usize) -> bool {
+        self.validate(p);
+        self.validate(q);
+        self.id[p] == self.id[q]
+    }
+
+    pub fn union(&mut self, p: usize, q: usize) {
+        self.validate(p);
+        self.validate(q);
+
+        let pid = self.id[p];
+        let qid = self.id[q];
+
+        if pid == qid {
+            return;
+        }
+
+        for i in 0..self.count {
+            if self.id[i] == pid {
+                self.id[i] = qid;
+            }
+        }
+        self.count -= 1;
+    }
+
+    pub fn find(&self, p: usize) -> usize {
+        self.validate(p);
+        self.id[p]
+    }
+
+    fn validate(&self, p: usize) {
+        if p >= self.count() {
+            panic!(
+                "Index out of range! Must be in between 0 and {}",
+                self.count - 1
+            );
+        }
+    }
+
+    pub fn print(&self) {
+        for item in &self.id {
+            println!("{}", item);
+        }
+    }
+
+    fn initialize_vec(n: usize) -> Vec<usize> {
+        let mut vec = vec![0; n];
+        for i in 0..n {
+            vec[i] = i;
+        }
+        vec
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn it_initializes() {
+        let qf = QuickFindUF::new(10);
+    }
+}
