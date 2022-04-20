8f13299ec7c0e9cb1d3b33b082b1bd52195c09fb
zain2323 <zainsiddiqui2323@gmail.com>
2022-04-20 06:26:44 +0500

    Added knuth shuffling algorithm

--- src/context/shuffle.rs
+++ src/context/shuffle.rs
@@ -1,21 +1,23 @@
 // Implements various shuffling algorithms
 
-use rand::{Rng, thread_rng};
+use rand::{thread_rng, Rng};
 
 trait Shuffler<T>
-where T: Ord
+where
+    T: Ord,
 {
-    fn shuffle(&mut self, slice: &mut[T]);
+    fn shuffle(&mut self, slice: &mut [T]);
 }
 
 pub struct KnuthShuffle;
 impl<T> Shuffler<T> for KnuthShuffle
-where T: Ord
+where
+    T: Ord,
 {
-    fn shuffle(&mut self, slice: &mut[T]) {
+    fn shuffle(&mut self, slice: &mut [T]) {
         let n = slice.len();
         for i in 0..n {
-            let random_num: usize = thread_rng().gen_range(0..i+1);
+            let random_num: usize = thread_rng().gen_range(0..i + 1);
             slice.swap(i, random_num);
         }
     }
@@ -26,15 +28,15 @@ mod test {
     use super::*;
     #[test]
     fn it_shuffles() {
-        let mut slice = vec![1,2,3,4,5];
+        let mut slice = vec![1, 2, 3, 4, 5];
         KnuthShuffle.shuffle(&mut slice);
-        assert_ne!(slice, &[1,2,3,4,5]);
+        assert_ne!(slice, &[1, 2, 3, 4, 5]);
     }
 
     #[test]
     fn it_shuffles_with_duplicates() {
-        let mut slice = vec![100,21,12,41,54,90,13,12];
+        let mut slice = vec![100, 21, 12, 41, 54, 90, 13, 12];
         KnuthShuffle.shuffle(&mut slice);
-        assert_ne!(slice, &[100,21,12,41,54,90,13,12]);
+        assert_ne!(slice, &[100, 21, 12, 41, 54, 90, 13, 12]);
     }
 }
